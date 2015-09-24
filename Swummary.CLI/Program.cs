deda90d8be207583eee34b17b558985415205066
Keith <herbertkb@vcu.edu>
2015-09-23 23:22:13 -0400

    added method to run srcml against sourcefiles listed on commandline

--- Swummary.CLI/Program.cs
+++ Swummary.CLI/Program.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;			// to run srcml as external process
+using System.ComponentModel;
 
 namespace Swummy.CLI
 {
@@ -20,12 +23,14 @@ namespace Swummy.CLI
 			}
 
 
-			// loop over files
 			foreach (string filename in files) 
 			{
 				// parse them with srcML
+				string srcmlOutput = RunSrcml (filename);
 
-				// extract class names 
+				Console.WriteLine (srcmlOutput);
+
+				// extract classes with all names that appear in that class with LINQ
 
 				// datastruct that maps class names to comments, methods, attributes
 
@@ -33,6 +38,27 @@ namespace Swummy.CLI
 			}
 			// pretty print the datastruct to console
 
+		}
+
+		/// <summary>
+		/// Runs srcml against a source code file. 
+		/// </summary>
+		/// <returns>A string for the output of srcml against the given sourcefile</returns>
+		/// <param name="sourcefile">Sourcefile to be analyzed</param>
+		private static string RunSrcml (string sourcefile) 
+		{
+			// derived from: 
+			// https://msdn.microsoft.com/en-us/library/system.diagnostics.process.standardoutput.aspx
+
+			var srcml = new Process ();
+			srcml.StartInfo.FileName = "srcml";
+			srcml.StartInfo.Arguments = sourcefile;
+			srcml.Start ();
+
+			StreamReader readStdOut = srcml.StandardOutput;
+			string output = readStdOut.ReadToEnd ();
+
+			return output;
 		}
 	}
 }
