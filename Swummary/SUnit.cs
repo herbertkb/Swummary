62a55eb5f19493ce3407f1ae9049fa8ed84fae54
Keith <herbertkb@vcu.edu>
2016-02-24 16:31:02 -0800

    Started making more constructors for SUnit, then realized I would need way too many to cover every situtation of whether an SUnit has a return type and/or arguments. So I erased and replaced with just one versatile constructor.

--- Swummary/SUnit.cs
+++ Swummary/SUnit.cs
@@ -7,33 +7,43 @@ namespace Swummary
         public SUnitType type;
         public string action;
         public string theme;
+
+        public bool hasArgs;
         public IEnumerable<string> args;
+
         public bool hasReturnType;
         public string returnType;
 
-        // Constructor for a single method call with no return type
-        public SUnit(string action, string theme, IEnumerable<string> args)
+        // Constructor for a SUnit of user-specified SUnitType with a return type
+        public SUnit(SUnitType type, string action, string theme, IEnumerable<string> args, string returnType)
         {
-            type = SUnitType.SingleMethodCall;
+            this.type = type;
             this.action = action;
             this.theme = theme;
             this.args = args;
 
-            hasReturnType = false;
-            returnType = null;
+            if (args != null)
+            {
+                this.args = args;
+                hasArgs = true;
+            }
+            else
+            {
+                this.args = null;
+                hasArgs = false;
+            }
+
+            if (returnType == null)
+            {
+                hasReturnType = true;
+                this.returnType = null;
+            }
+            else
+            {
+                hasReturnType = false;
+                this.returnType = null;
+            }
         }
-
-        // Constructor for a single method call with a return type
-        public SUnit(string action, string theme, IEnumerable<string> args, string returnType)
-        {
-            type = SUnitType.SingleMethodCall;
-            this.action = action;
-            this.theme = theme;
-            this.args = args;
-
-            hasReturnType = true;
-            this.returnType = returnType; 
-        } 
 
     }
 }
