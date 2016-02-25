b4e21cecbb002e3f51c807ce5cafec55baf17c96
Keith <herbertkb@vcu.edu>
2016-02-24 19:39:58 -0800

    Summarizer implementation, unit test, and DLLs.

--- Swummary/Summarizer.cs
+++ Swummary/Summarizer.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using OpenTextSummarizer;
 
 
 public static class Summarizer
 {
-    public static String Summarize(String text) { return ""; }
+    public static String Summarize(String text) {
+
+        var summaryArgs = new SummarizerArguments();
+
+        summaryArgs.DictionaryLanguage = "en";
+        //summaryArgs.DisplayLines = 3;
+        summaryArgs.DisplayPercent = 50;
+        summaryArgs.InputString = text;
+        
+
+        SummarizedDocument doc = OpenTextSummarizer.Summarizer.Summarize(summaryArgs);
+        var output = doc.Sentences;
+        var summary = string.Concat(output.ToArray());
+
+        return summary;
+
+    }
 
 }
