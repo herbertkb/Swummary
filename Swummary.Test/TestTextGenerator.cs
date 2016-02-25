c2d018b66966a4c084575a5ff5c36d282c56c84e
Keith <herbertkb@vcu.edu>
2016-02-24 18:19:42 -0800

    Filled in unit test for TextGenerator.

--- Swummary.Test/TestTextGenerator.cs
+++ Swummary.Test/TestTextGenerator.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using NUnit.Framework;
+using Swummary;
+using System.Collections.Generic;
 
 [TestFixture]
 public class TestTextGenerator
 {
 
-    // create a dummy WordNode List
-
-
     [TestCase]
-    public void TextGenerationFromTemplate() { Assert.AreEqual(1,2, "Not implemented."); }
+    public void TestTextGenerationForSingleMethodCall() {
+
+        SUnit sunit = new SUnit(SUnitType.SingleMethodCall, 
+                                "add", 
+                                "user", 
+                                new List<string> { "username", "password" },
+                                "void");
+
+        var sentence = TextGenerator.GenerateText(sunit);
+
+        Assert.AreEqual("Add user given username and password.", sentence);
+         
+    }
 
 }
